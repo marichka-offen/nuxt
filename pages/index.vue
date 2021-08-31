542f1002936f63c9f6fbe6bedceab7e1712e66e1
Marichka Offen <marichka.offen@gmail.com>
2021-08-31 17:49:17 -0400

    setting SEO with vue-meta

--- pages/index.vue
+++ pages/index.vue
@@ -1,7 +1,19 @@
 <template>
-  <Tutorial/>
+  <div>
+      <h1>Events</h1>
+  </div>
 </template>
 
 <script>
-export default {}
-</script>
+    export default {
+      head() { // <-- property used by vue-meta to add header tags
+        return {
+          title: 'Event Listing', // <-- For our title tag
+        }
+      }
+    }
+    </script>
+
+<style>
+
+</style>
